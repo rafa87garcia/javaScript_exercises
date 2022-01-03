d91da4e9c65119eac78c934ba89a4ad86d669e30
Rafael García Rea <rgrea@amara.es>
2022-01-04 00:15:34 +0100

    Añadir ejercicio 8

---
+++ ej_08.js
@@ -0,0 +1,11 @@
+const miObjeto = {
+  city: "Sacramento",
+  state: "California",
+  country: "USA",
+  continent: "North America",
+};
+
+console.log(miObjeto);
+delete miObjeto.country;
+
+console.log(miObjeto);
